4e1a52adf99fe1a4ab807bff7acec5fcd64aae6b
soumik <soumikcse07@gmail.com>
2026-02-03 01:08:43 +0600

    feat: Add SMK TV page and integrate its Flutter web application.

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Index from "./pages/Index";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import Admin from "./pages/Admin";
+import SmkTv from "./pages/SmkTv";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -25,6 +26,7 @@ const App = () => (
             <Route path="/blog" element={<Blog />} />
             <Route path="/blog/:slug" element={<BlogPost />} />
             <Route path="/admin" element={<Admin />} />
+            <Route path="/smk-tv" element={<SmkTv />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>
